9f848b59ca93db308724ce45e640c2857d8f2783
andrezza.souza <andrezza.souza@zup.com.br>
2021-11-28 13:14:04 -0300

    Retornando userId

--- src/main/java/br/com/unitri/graphicsLoginApi/controller/UserAuthenticationController.java
+++ src/main/java/br/com/unitri/graphicsLoginApi/controller/UserAuthenticationController.java
@@ -1,9 +1,14 @@
 package br.com.unitri.graphicsLoginApi.controller;
 
+import br.com.unitri.graphicsLoginApi.models.GraphicUser;
 import br.com.unitri.graphicsLoginApi.models.dto.request.LoginRequest;
+import br.com.unitri.graphicsLoginApi.models.dto.response.AuthenticationResponse;
+import br.com.unitri.graphicsLoginApi.repositories.UserRepository;
 import br.com.unitri.graphicsLoginApi.security.TokenManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @RequestMapping( "/api/auth" )
@@ -23,6 +29,8 @@ public class UserAuthenticationController {
 
     private AuthenticationManager authManager;
     private TokenManager tokenManager;
+    @Autowired
+    private UserRepository userRepository;
 
     public UserAuthenticationController (AuthenticationManager authManager , TokenManager tokenManager) {
         this.authManager = authManager;
@@ -33,12 +41,18 @@ public class UserAuthenticationController {
             .getLogger(UserAuthenticationController.class);
 
     @PostMapping( consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE )
-    public ResponseEntity<String> authenticate (@RequestBody @Valid LoginRequest request ) {
+    public ResponseEntity<AuthenticationResponse> authenticate (@RequestBody @Valid LoginRequest request ) {
         UsernamePasswordAuthenticationToken authenticationToken = request.build();
         try {
             Authentication authentication = authManager.authenticate(authenticationToken);
             String jwt = tokenManager.generateToken(authentication);
-            return ResponseEntity.ok(jwt);
+            Optional<GraphicUser> user = userRepository.findByLogin(request.getEmail());
+            if(!user.isPresent()){
+                return ResponseEntity.notFound().build();
+            }
+            Long userId = user.get().getId();
+            return ResponseEntity.ok(new AuthenticationResponse(jwt, userId));
+
         } catch (AuthenticationException e) {
             log.error("[Autenticacao] {}" , e);
             return ResponseEntity.badRequest().build();
---
+++ src/main/java/br/com/unitri/graphicsLoginApi/models/dto/response/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package br.com.unitri.graphicsLoginApi.models.dto.response;
+
+public class AuthenticationResponse {
+
+    private String jwt;
+    private Long userId;
+
+    public AuthenticationResponse(String jwt, Long userId) {
+        this.jwt = jwt;
+        this.userId = userId;
+    }
+
+
+}
