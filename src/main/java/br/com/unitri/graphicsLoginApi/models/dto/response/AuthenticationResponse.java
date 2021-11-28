5a75896299b34f415cc0159a00e34fb7781aa29c
andrezza.souza <andrezza.souza@zup.com.br>
2021-11-28 18:22:32 -0300

    Corrigindo bug e retornando userId

---
+++ src/main/java/br/com/unitri/graphicsLoginApi/models/dto/response/AuthenticationResponse.java
@@ -0,0 +1,20 @@
+package br.com.unitri.graphicsLoginApi.models.dto.response;
+
+public class AuthenticationResponse {
+
+    private String jwt;
+    private Long userId;
+
+    public AuthenticationResponse(String jwt, Long userId) {
+        this.jwt = jwt;
+        this.userId = userId;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+}
